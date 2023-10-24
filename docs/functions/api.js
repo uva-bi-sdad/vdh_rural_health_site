a53ebf31297352892829a89dd1bc941af6f7b0e1
alan <ahw9f@virginia.edu>
2023-10-24 12:19:17 -0400

    patch: updating basemap

--- docs/functions/api.js
+++ docs/functions/api.js
@@ -4,7 +4,7 @@ settings.metadata.info = {}
 const dp = require('../data/datapackage.json')
 if (dp.measure_info) settings.metadata.measure_info = dp.measure_info
 dp.resources.forEach(r => (settings.metadata.info[r.name] = r))
-const DataHandler = require('../data_handler.v2.min.js'),
+const DataHandler = require('../data_handler.v1.min.js'),
   data = new DataHandler(settings, void 0, {
     tract: require('../tract.json'),
     county: require('../county.json'),
